587831ee3ac31531133ab2f869b12703f66c2a2d
xiaoyao.qiu <xiaoyao.qiu@estudiantat.upc.edu>
2026-04-28 13:32:10 +0200

    firebase funciona amb el back

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,7 +46,7 @@ flutter {
 
 dependencies {
     // Import the Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:34.11.0"))
+    implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
 
 
     // TODO: Add the dependencies for Firebase products you want to use
